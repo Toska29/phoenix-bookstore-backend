1d917d5b30eb0169dc14024d2d5ef87d906db180
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-24 11:44:45 +0100

    bookstore project

---
+++ src/main/java/africa/semicolon/phoenixbookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package africa.semicolon.phoenixbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/africa/semicolon/phoenixbookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.semicolon.phoenixbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
