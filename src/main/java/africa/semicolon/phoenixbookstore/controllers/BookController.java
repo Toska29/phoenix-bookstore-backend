fc366ce95aafd4a43161370bc16a329950574bab
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-25 12:21:39 +0100

    update

--- src/main/java/africa/semicolon/phoenixbookstore/controllers/BookController.java
+++ src/main/java/africa/semicolon/phoenixbookstore/controllers/BookController.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
+
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
