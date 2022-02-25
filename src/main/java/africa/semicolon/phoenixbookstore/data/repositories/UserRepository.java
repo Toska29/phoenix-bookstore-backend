35111e70aef8200c47180410e94d792b40f0956d
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-25 10:12:17 +0100

    User repos

--- src/main/java/africa/semicolon/phoenixbookstore/data/repositories/UserRepository.java
+++ src/main/java/africa/semicolon/phoenixbookstore/data/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package africa.semicolon.phoenixbookstore.data.repositories;
 import africa.semicolon.phoenixbookstore.data.models.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<AppUser, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByEmail(String email);
 }
