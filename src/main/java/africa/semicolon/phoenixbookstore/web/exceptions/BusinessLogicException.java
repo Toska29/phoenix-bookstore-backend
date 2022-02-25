751090d2336f30922c60e9983511d107994e1ad0
Oludairo <eoludairo61@gmail.com>
2022-02-25 05:34:57 +0100

    Business logic exception

---
+++ src/main/java/africa/semicolon/phoenixbookstore/web/exceptions/BusinessLogicException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.phoenixbookstore.web.exceptions;
+
+public class BusinessLogicException extends Throwable {
+    public BusinessLogicException(String message) {
+        super(message);
+    }
+}
